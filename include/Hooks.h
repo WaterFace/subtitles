b9df894529c2ee5a4f6e1d4798bcacbabe932921
WaterFace <kevinvisserweckworth@gmail.com>
2024-03-08 15:42:50 -0500

    also override vanilla calls to "ShowSubtitle", further reducing flicker

--- include/Hooks.h
+++ include/Hooks.h
@@ -29,26 +29,36 @@ namespace Subtitles
 		static inline REL::Relocation<decltype(UpdatePCMod)> UpdatePC;
 	};
 
-	// Hook into the Invoke call in HUDMenu::ProcessMessage which calls "HideSubtitle"
+	// Hook into the Invoke calls in HUDMenu::ProcessMessage which call "HideSubtitle" and "ShowSubtitle"
 	class InvokeHook
 	{
 	public:
 		static void Install()
 		{
 			auto address = REL::VariantID(50718, 51612, 0).address();
+
 			auto offset = REL::VariantOffset(0x756, 0x77E, 0).offset();
-			SKSE::GetTrampoline().write_call<5>(address + offset, InvokeMod);
-			logger::info("Invoke call in HUDMenu::ProcessMessage hooked");
+			SKSE::GetTrampoline().write_call<5>(address + offset, InvokeModHide);
+
+			offset = REL::VariantOffset(0x703, 0x72B, 0).offset();
+			SKSE::GetTrampoline().write_call<5>(address + offset, InvokeModShow);
+			logger::info("Invoke calls in HUDMenu::ProcessMessage hooked");
 		}
 
 	private:
-		static bool InvokeMod(RE::GFxValue::ObjectInterface* objInt, void* a_data, RE::GFxValue* a_result, const char* a_name, const RE::GFxValue* a_args, RE::UPInt a_numArgs, bool isDObj)
+		static bool InvokeModHide(RE::GFxValue::ObjectInterface* objInt, void* a_data, RE::GFxValue* a_result, const char* a_name, const RE::GFxValue* a_args, RE::UPInt a_numArgs, bool isDObj)
 		{
 			// We have to check this since multiple message types pass through this branch
 			if (strcmp(a_name, "HideSubtitle") == 0) {
 				return true;
 			}
 
+			return objInt->Invoke(a_data, a_result, a_name, a_args, a_numArgs, isDObj);
+		}
+
+		static bool InvokeModShow(RE::GFxValue::ObjectInterface* objInt, void* a_data, RE::GFxValue* a_result, const char* a_name, const RE::GFxValue* a_args, RE::UPInt a_numArgs, bool isDObj)
+		{
+			// We don't have to check here since this one only handles "ShowSubtitle" invocations
 			return objInt->Invoke(a_data, a_result, a_name, a_args, a_numArgs, isDObj);
 		}
 	};
