58b1f8da4a3cbe07c76069290ba6d04077e0046e
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2024-03-09 20:04:00 +0200

    Abstracted out names retrieval and added NND API to get appropriate generated name.

---
+++ src/NPCNameProvider.cpp
@@ -0,0 +1,29 @@
+#include "NPCNameProvider.h"
+#include "NND_API.h"
+
+const char* NPCNameProvider::GetName(const RE::Actor* actor) const
+{
+	if (NND) {
+		if (auto name = NND->GetName(actor, NND_API::NameContext::kSubtitles); !name.empty()) {
+			return name.data();
+		}
+	}
+
+	if (auto xTextData = actor->extraList.GetByType<RE::ExtraTextDisplayData>(); xTextData) {
+		return xTextData->displayName.c_str();
+	}
+
+	return actor->GetName();
+}
+
+void NPCNameProvider::RequestAPI()
+{
+	if (!NND) {
+		NND = static_cast<NND_API::IVNND1*>(NND_API::RequestPluginAPI(NND_API::InterfaceVersion::kV1));
+		if (NND) {
+			logger::info("Obtained NND API - {0:x}", reinterpret_cast<uintptr_t>(NND));
+		} else {
+			logger::info("Failed to obtain NND API");
+		}
+	}
+}
